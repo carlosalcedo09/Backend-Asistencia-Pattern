e7180287a2df618f79285a15b0c8f116984bb0a2
Carlos Alcedo <carlosalcedojavier@gmail.com>
2025-12-10 22:09:20 -0500

    Fix staticfiles for Render 2

--- wapp/settings.py
+++ wapp/settings.py
@@ -94,7 +94,7 @@ MIDDLEWARE = [
 ]
 
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 CORS_ALLOWED_ORIGINS = [
